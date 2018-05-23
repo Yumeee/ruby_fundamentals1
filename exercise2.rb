32859151aa2065119e8b209c46189b6737a21f54
YUMEE <yumeehk@hotmail.com>
2018-05-22 21:47:40 -0400

    exercise 2 done

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# 1.
+puts 55 * 0.15
+
+# 2
+puts "I have " + 12.to_s + " dollars."
+
+# 3
+puts "The multiplication of 45628 and 7839 gives a result of #{45682*7839}."
+
+# 4
+puts (10<20&&30<20) || !(10==11)
